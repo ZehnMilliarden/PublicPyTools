281a726dfe5a4cb52d9c051f594ca8d2d928542d
lxf <git@lxf.science>
2021-11-10 08:40:22 +0800

    [zmyy]代码调整，敏感信息本地配置化

--- zmyy/zmyy_b.py
+++ zmyy/zmyy_b.py
@@ -9,44 +9,59 @@ import os
 import sys
 
 class czmyy_test:
-    def __init__(self):
-        # 延迟时间
-        self.int_time = 4
-        # Cookies
-        self.cookies = 'ASP.NET_SessionId=qfzodsgrd1kl4jmkjzspn3k0'
-        self.headers = {
-            'charset': 'utf-8',
-            'Accept-Encoding': 'gzip, deflate, br',
-            'referer': 'https://servicewechat.com/wx2c7f0f3c30d99445/73/page-frame.html',
-            'cookie': self.cookies,
-            'content-type': 'application/json',
-            'User-Agent': 'Mozilla/5.0 (Linux; Android 9; Redmi Note 7 Build/PKQ1.180904.001; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/74.0.3729.136 Mobile Safari/537.36 MicroMessenger/7.0.6.1460(0x27000634) Process/appbrand0 NetType/4G Language/zh_CN',
-            'Host': 'cloud.cn2030.com',
-            'Connection': 'Keep-Alive',
-            'zftsl': '739a71bd55e8e6b56afcf1d982c10d7e'
-        }
 
+    def __init_config__(self):
         proDir = os.path.split(os.path.realpath(__file__))[0]
         # 在当前文件路径下查找.ini文件
         configPath = os.path.join(proDir, "config.ini")
         self.conf = configparser.ConfigParser()
-        self.conf.read(configPath)
+        self.conf.read(configPath, encoding='utf-8-sig')
+        self.confzmyy = self.conf['zmyy']
 
-        # 猜测验证码的X
-        self.x = '33'
+    # 关键敏感信息本地配置
+    def __init_var__(self):
+        # 延迟时间
+        self.delaytime = 4
+        #
         self.Checks = True
+        # Cookies
+        self.yi_cookies = self.confzmyy.get('cookies')
+        # 请求头
+        self.headers = {
+            'charset': 'utf-8',
+            'Accept-Encoding': 'gzip, deflate, br',
+            'referer': 'https://servicewechat.com/wx2c7f0f3c30d99445/73/page-frame.html',
+            'cookie': self.yi_cookies,
+            'content-type': 'application/json',
+            'User-Agent': 'Mozilla/5.0 (Linux; Android 9; Redmi Note 7 Build/PKQ1.180904.001; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/74.0.3729.136 Mobile Safari/537.36 MicroMessenger/7.0.6.1460(0x27000634) Process/appbrand0 NetType/4G Language/zh_CN',
+            'Host': 'cloud.cn2030.com',
+            'Connection': 'Keep-Alive',
+            'zftsl': self.confzmyy.get('zftsl')
+        }
+        # 猜测验证码的X坐标？
+        self.yi_xsite = self.confzmyy.getint('xsite')
+        # 验证码的有坐标
+        self.yi_ysite = self.confzmyy.getint('ysite')
         # 填写预约时间
-        self.yuyue_times = self.conf.get('zmyy','yuyuetimes')
-        # 1= 九价
-        self.p_id = '1'
+        self.yi_yytimes = self.confzmyy.get('yuyuetimes')
+        # 疫苗种类
+        self.yi_ymid = self.confzmyy.getint('ymid')
         # 生日
-        self.yi_birthday = self.conf.get('zmyy', 'birthday')
+        self.yi_birthday = self.confzmyy.get('birthday')
         # 身份证
-        self.yi_cdid = self.conf.get('zmyy', 'cdid')
+        self.yi_cdid = self.confzmyy.get('cdid')
         # 电话
-        self.yi_telphone = self.conf.get('zmyy', 'telphone')
+        self.yi_telphone = self.confzmyy.get('telphone')
         # 名字
-        self.yi_name = self.conf.get('zmyy', 'name')
+        self.yi_name = self.confzmyy.get('name')
+        # 城市ID
+        self.yi_cityid = self.confzmyy.getint('cityid')
+        # 性别
+        self.yi_sex = self.confzmyy.getint('sex')
+
+    def __init__(self):
+        self.__init_config__()
+        self.__init_var__()
 
     def generate_random_str(randomlength):
         random_str = ''
@@ -57,13 +72,13 @@ class czmyy_test:
         return random_str
 
     def GetSubscribe(self):
-        print('（1）开始访问获取客户订阅日期详细信息：GetCustSubscribeDateDetail')
-        time.sleep(self.int_time)
+        print(u'（1）开始访问获取客户订阅日期详细信息：GetCustSubscribeDateDetail')
+        time.sleep(self.delaytime)
         payload = {
             'act': 'GetCustSubscribeDateDetail',
-            'pid': self.p_id,
-            'id': '243',
-            'scdate': self.yuyue_times,
+            'pid': self.yi_ymid,
+            'id': self.yi_cityid,
+            'scdate': self.yi_yytimes,
         }
         code = requests.get(
             url="https://cloud.cn2030.com/sc/wx/HandlerSubscribe.ashx",
@@ -96,8 +111,8 @@ class czmyy_test:
 
     def Submit(self):
         # 获取验证码
-        print('（2）访问验证码：GetCaptcha')
-        time.sleep(self.int_time)
+        print(u'（2）访问验证码：GetCaptcha')
+        time.sleep(self.delaytime)
         payload = {
             'act': 'GetCaptcha',
         }
@@ -106,7 +121,7 @@ class czmyy_test:
             headers=self.headers, params=payload, verify=False)
         # 转json
         if int(code.status_code) == 200:
-            ck_s = self.YanZheng302()
+            ck_s = self.__CaptchaVerify()
             return ck_s
         else:
             print('访问异常,继续访问%s' % code.status_code)
@@ -114,13 +129,13 @@ class czmyy_test:
             return ck_s
 
     def GetCustSubscribeDateDetail(self):
-        print('访问获取客户订阅日期详细信息：GetCustSubscribeDateDetail')
+        print(u'访问获取客户订阅日期详细信息：GetCustSubscribeDateDetail')
         time.sleep(self.int_time)
         payload = {
             'act': 'GetCustSubscribeDateDetail',
-            'pid': self.p_id,
-            'id': '243',
-            'scdate': self.yuyue_times,
+            'pid': self.yi_ymid,
+            'id': self.yi_cityid,
+            'scdate': self.yi_yytimes,
         }
 
         code = requests.get(
@@ -143,18 +158,19 @@ class czmyy_test:
         else:
             print(code.status_code)
             check_mxid = True
-            print('访问异常,继续访问')
+            print(u'访问异常,继续访问')
             mxid = ''
             return check_mxid, mxid
 
-    def YanZheng302(self):
-        print("（3）开始验证：CaptchaVerify")
-        time.sleep(self.int_time)
+    # 验证码处理
+    def __CaptchaVerify(self):
+        print(u"（3）开始验证：CaptchaVerify")
+        time.sleep(self.delaytime)
         payload = {
             'act': 'CaptchaVerify',
             'token': '',
-            'x': x,
-            'y': '5',
+            'x': self.yi_xsite,
+            'y': self.yi_ysite,
         }
 
         code = requests.get(
@@ -166,35 +182,35 @@ class czmyy_test:
             print(code_json_dict)
             if int(code_json_dict['status']) != 204 and int(code_json_dict['status']) != 201:
                 if int(code_json_dict['status']) != 408:
-                    print('验证码-验证成功')
-                    print('guid:%s' % code_json_dict['guid'])
+                    print(u'验证码-验证成功')
+                    print(u'guid:%s' % code_json_dict['guid'])
                     guid = code_json_dict['guid']
                     Checks = False
                     # 成功后去访问Save20结果去提交数据=预约成功
-                    print('（4）马上提交')
-                    self.Save20(yuyue_times, p_id, mxid, guid)
+                    print(u'（4）马上提交')
+                    self.Save20(guid)
                     return Checks
                 else:
                     Checks = True
-                    print('请重新授权Cookies')
+                    print(u'请重新授权Cookies')
                     return Checks
             else:
                 Checks = True
-                print('继续验证')
+                print(u'继续验证')
                 return Checks
 
-    def Save20(self, times, p_id, mxid, guid):
+    def Save20(self, guid):
         payload = {
             'act': 'Save20',
             'birthday': self.yi_birthday,
             'tel': self.yi_telphone,
-            'sex': "2", # 性别2 女  1 男
+            'sex': self.yi_sex, # 性别2 女  1 男
             'cname': self.yi_name,
             'doctype': "1",
             'idcard': self.yi_cdid,
-            'mxid': mxid,
-            'date': times,
-            'pid': p_id,
+            'mxid': self.mxid,
+            'date': self.yi_yytimes,
+            'pid': self.yi_ymid,
             'Ftime': "1",
             'guid': guid,
         }
@@ -207,7 +223,7 @@ class czmyy_test:
         print(json_dict)
         if int(json_dict['status']) != 201:
             print(json_dict)
-            print('预约成功')
+            print(u'预约成功')
         else:
             pass
 
